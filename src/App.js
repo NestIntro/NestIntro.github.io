f7c7346b159751fdb8c26163adc64a528f33f63e
김지환 <91196025+Kimjipang@users.noreply.github.com>
2023-08-18 15:41:13 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import { Box, Code, Image, Input, Text } from "@chakra-ui/react";
+import { Box, Code, Image, Img, Input, Text } from "@chakra-ui/react";
 import background from "./assets/images/background.jpeg";
 import title from "./assets/images/title.png";
 import subtitle from "./assets/images/subtitle.png";
 import bubble from "./assets/images/bubble.png";
 import installation from "./assets/images/installation.png";
 import basicusage from "./assets/images/basic-usage.png";
-// import spongebob from "./assets/images/spongebob.png";
-// import patrick from "./assets/images/patrick.png";
-// import squidward from "./assets/images/squidward.png";
-// import plankton from "./assets/images/plankton.png";
-// import gary from "./assets/images/gary.png";
-// import krabs from "./assets/images/krabs.png";
+import spongebob from "./assets/images/spongebob.png";
+import patrick from "./assets/images/patrick.png";
+import squidward from "./assets/images/squidward.png";
+import plankton from "./assets/images/plankton.png";
+import gary from "./assets/images/gary.png";
+import krabs from "./assets/images/krabs.png";
+import sandy from "./assets/images/sandy.png";
 
 export default function App() {
   return (
@@ -18,6 +19,7 @@ export default function App() {
       <Box
         // width="100vw"
         // height="100vh"
+        position={"relative"}
         backgroundImage={background}
         backgroundSize="cover"
         backgroundPosition="center"
@@ -75,6 +77,72 @@ export default function App() {
               >
                 $node node_modules/magic-conch-soragodong/cli.js{" "}
               </Text>
+              <Img
+                src={spongebob}
+                position="absolute"
+                top="25%"
+                right="-70%"
+                w="250px"
+                h="250px"
+                transform="rotate(-20deg) scaleX(-1)"
+              />
+              <Img
+                src={krabs}
+                position="absolute"
+                bottom="80%"
+                right="137%"
+                w="300px"
+                h="300px"
+                transform="rotate(10deg)"
+              />
+              <Img
+                src={gary}
+                position="absolute"
+                // bottom="70%"
+                // right="120%"
+                top="-176%"
+                right="105%"
+                w="90px"
+                // h="100px"
+                transform="rotate(-20deg)"
+              />
+              <Img
+                src={patrick}
+                position="absolute"
+                top="20%"
+                right="120%"
+                w="300px"
+                h="300px"
+                transform="rotate(-20deg)"
+              />
+              <Img
+                src={squidward}
+                position="absolute"
+                bottom="80%"
+                left="150%"
+                w="220px"
+                // h="250px"
+                transform="rotate(10deg)"
+              />
+              <Img
+                src={sandy}
+                position="absolute"
+                bottom="70%"
+                left="80%"
+                // w="220px"
+                h="300px"
+                transform="rotate(-10deg)"
+              />
+
+              <Img
+                src={plankton}
+                position="absolute"
+                bottom="220%"
+                left="125%"
+                w="150px"
+                h="150px"
+                transform="rotate(-40deg) scaleX(-1)"
+              />
             </Box>
           </Box>
         </Box>
